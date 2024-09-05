fdbab848594b364e4a26cc88a89a32f5978fbf2c
Dong Jin <ceciliajin2019@gmail.com>
2024-09-05 11:16:26 +0800

    使用RTK管理账单列表

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from "react-router-dom"
 import router from "@/router"
+import { Provider } from 'react-redux'
+import store from './store'
+
 // 别名路径
 import sum from '@/test'
-
+// 导入定制主题文件
 import "./theme.css"
 
 const total = sum(1, 3)
@@ -14,7 +17,9 @@ console.log(total)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={router}/>
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 );
 
 
